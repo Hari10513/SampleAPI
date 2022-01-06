60cae45f3658701f6b88f9d97ff4d96427c6169b
Raghul prasanth A <rprasant@starbucks.com>
2022-01-06 12:27:47 +0530

    Add project files.

---
+++ SampleAPI/Controllers/SampleController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        public List<Model> Models = new List<Model>()
+        {
+            new Model { Id = 1, Title = "The Girl on the train", Author = "Hawkins, Paula", PublicationYear = 2017, CallNumber = "7904932295"},
+            new Model { Id = 2, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Model { Id = 3, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Model { Id = 4, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Model { Id = 5, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Model { Id = 6, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Model { Id = 7, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Model { Id = 8, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Model { Id = 9, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Model { Id = 10, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"}
+        };
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Model>> GetAllModels()
+        {
+            return Models;
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Model> GetModel(int id)
+        {
+            var Model = Models.FirstOrDefault(x => x.Id == id);
+            if (Model == null)
+            {
+                return NotFound();
+            }
+            return Model;
+        }
+
+    }
+}
---
+++ SampleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Value1", "Value2", "Value3", "Value4", "Value5" };
+        }
+
+        [HttpGet("{id}")]
+
+        public string Get(int id)
+        {
+            return "The value is " + id;
+        }
+
+    }
+}
+    
+
